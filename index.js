7287820a12b574813f9d91c1da628b3ac6bdc391
Yuri Rech Alves <yurirech2003@gmail.com>
2023-04-17 21:54:09 -0300

    Editado um erro de sequência de arquivo no index.js

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const express = require ("express");
 const routes = require ("./rotas");
 
 const server = express();
-
-server.use(routes);
 server.use(express.json());
 server.use(express.urlencoded({extended:true}));
+server.use(routes);
 
 server.listen(3000, () => {console.log("Servidor em execução: localhost/3000")});
-
